b531c18f43683cf0389f505a8513ee02aa0ab4f9
laur sandu <Sapistudio@users.noreply.github.com>
2019-11-02 16:37:13 +0200

    Update XmlParser.php

--- src/Parsers/XmlParser.php
+++ src/Parsers/XmlParser.php
@@ -3,25 +3,8 @@ namespace SapiStudio\FileSystem\Parsers;
 
 class XmlParser extends Parser
 {
-    private $xml;
-    
-    /**
-     * XmlParser::objectify()
-     * 
-     * @param mixed $value
-     * @return
-     */
-    private function objectify($value)
-    {
-        $temp = is_string($value) ? simplexml_load_string("<?xml version='1.0' standalone='yes'?><data>" .preg_replace('/^.+\n/', '', $value) . "</data>", 'SimpleXMLElement',LIBXML_NOCDATA) : $value;
-        $result = [];
-        foreach ((array )$temp as $key => $value)
-        {
-            $result[$key] = (is_array($value) or is_object($value)) ? $this->objectify($value) :
-                $value;
-        }
-        return $result;
-    }
+    private $xmlObject;
+    private $simpleXml;
     
     /**
      * XmlParser::__construct()
@@ -31,7 +14,22 @@ class XmlParser extends Parser
      */
     public function __construct($data)
     {
-        $this->xml = $this->objectify($data);
+        $this->simpleXml = simplexml_load_string("<?xml version='1.0' standalone='yes'?><data>" .preg_replace('/^.+\n/', '', $data) . "</data>", 'SimpleXMLElement',LIBXML_NOCDATA);
+        $this->xmlObject = $this->objectify($this->simpleXml);
+    }
+    
+    /**
+     * XmlParser::objectify()
+     * 
+     * @param mixed $value
+     * @return
+     */
+    private function objectify($value)
+    {
+        $result = [];
+        foreach ((array)$value as $key => $valueData)
+            $result[$key] = (is_array($valueData) or is_object($valueData)) ? $this->objectify($valueData) : $valueData;
+        return $result;
     }
     
     /**
@@ -41,6 +39,16 @@ class XmlParser extends Parser
      */
     public function toArray()
     {
-        return (array )$this->xml;
+        return (array)$this->xmlObject;
+    }
+    
+    /**
+     * XmlParser::toArray()
+     * 
+     * @return
+     */
+    public function toObject()
+    {
+        return $this->simpleXml;
     }
 }
