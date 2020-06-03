765b8acb7171ac6fb456856202bfc4a80c027556
laur sandu <Sapistudio@users.noreply.github.com>
2020-06-04 02:15:32 +0300

    Update Handler.php

--- src/Handler.php
+++ src/Handler.php
@@ -11,34 +11,20 @@ class Handler
     /** iluminate filesystem object*/
     protected static $filesystem = null;
     
-    /**
-     * Handler::__construct()
-     * 
-     * @return
-     */
+    /** Handler::__construct() */
     public function __construct()
     {
         self::bootIluminateFileSystem();
     }
 
-    /**
-     * Handler::__callStatic()
-     * 
-     * @param mixed $method
-     * @param mixed $parameters
-     * @return
-     */
+    /** Handler::__callStatic() */
     public static function __callStatic($method, $parameters)
     {
         self::bootIluminateFileSystem();
         return static::$filesystem->$method(...$parameters);
     }
     
-    /**
-     * Handler::bootIluminateFileSystem()
-     * 
-     * @return
-     */
+    /** Handler::bootIluminateFileSystem() */
     protected static function bootIluminateFileSystem()
     {
         if (!isset(static::$filesystem)) {
@@ -46,36 +32,19 @@ class Handler
         }
     }
     
-    /**
-     * Handler::getFinder()
-     * 
-     * @return
-     */
+    /** Handler::getFinder()*/
     public static function getFinder(){
         return Finder::create();
     }
     
-    /**
-     * Handler::createDir()
-     * 
-     * @param mixed $dir
-     * @param integer $mkdirPermissions
-     * @param bool $recursive
-     * @return
-     */
+    /** Handler::createDir()*/
     public static function createDir($dir, $mkdirPermissions = 0755, $recursive=false){
         if (is_dir($dir))
             return true;
         static::$filesystem->makeDirectory($dir,$mkdirPermissions,$recursive);
     }
 
-    /**
-     * Handler::getAllFiles()
-     * 
-     * @param mixed $dir
-     * @param bool $returnfile
-     * @return
-     */
+    /** Handler::getAllFiles() */
     public static function getAllFiles($dir,$returnfile = false)
     {
         if(!is_dir($dir))
@@ -88,71 +57,41 @@ class Handler
         return $return;
     }
 
-    /**
-     * Handler::getDirectoriesAndFiles()
-     * 
-     * @param mixed $dir
-     * @return
-     */
+    /** Handler::getDirectoriesAndFiles()*/
     public static function getDirectoriesAndFiles($dir)
     {
         return array_merge(static::$filesystem->directories($dir), static::$filesystem->files($dir));
     }
     
-    /**
-     * Handler::dumpFile()
-     * 
-     * @param mixed $path
-     * @param mixed $text
-     * @return
-     */
+    /** Handler::dumpFile()*/
     public static function dumpFile($path,$text){
         //$this->chmod($path,0755);
         chmod($path, 0755);
         return static::$filesystem->put($path,$text);
     }
     
-    /**
-     * Handler::append()
-     * 
-     * @param mixed $path
-     * @param mixed $data
-     * @return
-     */
+    /** Handler::dumpJson() */
+    public static function dumpJson($path,$text){
+        return self::dumpFile($path,json_encode($text));
+    }
+    
+    /** Handler::dumpJson() */
+    public static function dumpToConfig($path,$configData){
+        return self::dumpFile($path,self::var_export_config($configData));
+    }
+    
+    /** Handler::append() */
     public static function append($path, $data)
     {
         return static::$filesystem->append($path,"\n".$data);
     }
     
-    /**
-     * Handler::appendJson()
-     * 
-     * @param mixed $path
-     * @param mixed $text
-     * @return
-     */
+    /** Handler::appendJson() */
     public static function appendJson($path,$entries=[]){
         return self::dumpJson($path,(new Collection(self::loadJson($path,true)))->concat($entries)->toArray());
     }
     
-    /**
-     * Handler::dumpJson()
-     * 
-     * @param mixed $path
-     * @param mixed $text
-     * @return
-     */
-    public static function dumpJson($path,$text){
-        return self::dumpFile($path,json_encode($text));
-    }
-    
-    /**
-     * Handler::loadJson()
-     * 
-     * @param mixed $path
-     * @param bool $toArray
-     * @return
-     */
+    /** Handler::loadJson() */
     public static function loadJson($path,$toArray=false){
         try{
             return json_decode(static::$filesystem->get($path),$toArray);
@@ -161,12 +100,7 @@ class Handler
         }
     }
     
-    /**
-     * Handler::loadJsonAsConfig()
-     * 
-     * @param mixed $path
-     * @return
-     */
+    /** Handler::loadJsonAsConfig()*/
     public static function loadJsonAsConfig($path){
         try{
             $jsonData = self::loadJson($path,true);
@@ -176,13 +110,7 @@ class Handler
         }
     }
     
-    /**
-     * Handler::fileToArray()
-     * 
-     * @param mixed $path
-     * @param bool $toArray
-     * @return
-     */
+    /** Handler::fileToArray() */
     public static function fileToArray($path,$separator="\n"){
         try{
             return explode($separator,static::$filesystem->get($path));
@@ -191,18 +119,30 @@ class Handler
         }
     }
     
-    /**
-     * Handler::arrayToFile()
-     * 
-     * @param mixed $path
-     * @param mixed $data
-     * @return
-     */
+    /** Handler::arrayToFile() */
     public static function arrayToFile($path,$data=[]){
         try{
             return self::dumpFile($path,implode("\n",$data));
         }catch(\Exception $e){
             return [];
         }
+    }
+    
+    /**
+     * Handler::var_export_config()
+     * convert PHP's array() syntax to PHP 5.4's short array syntax [],and modified result for config file
+     * @return
+     */
+    public static function var_export_config($configData = [],$indent = 4) {
+        $object = json_decode(str_replace(['(', ')'], ['&#40', '&#41'], json_encode($configData)), true);
+        $export = str_replace(['array (', ')', '&#40', '&#41'], ['[', ']', '(', ')'], var_export($object, true));
+        $export = preg_replace("/ => \n[^\S\n]*\[/m", ' => [', $export);
+        $export = preg_replace("/ => \[\n[^\S\n]*\]/m", ' => []', $export);
+        $spaces = str_repeat(' ', $indent);
+        $export = preg_replace("/([ ]{2})(?![^ ])/m", $spaces, $export);
+        $export = preg_replace("/^([ ]{2})/m", $spaces, $export);
+        $export = str_ireplace("'false'", "false", $export);
+        $export = str_ireplace("'true'", "true", $export);
+        return '<?php '."\n".'return '.$export.';';
     }
 }
